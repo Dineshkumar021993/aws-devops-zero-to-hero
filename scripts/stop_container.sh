77f42727813e737369922161a5eb0aa187a7b3e1
Dinesh kumar <90820982+Dineshkumar021993@users.noreply.github.com>
2025-02-18 15:01:30 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,4 @@
 set -e
 
 # Stop the running container (if any)
-sudo docker stop -d -p 6000:9000 dineshkumar021993/samplepythonawscodedeploy
+sudo docker stop -d -p 6000:8000 dineshkumar021993/samplepythonawscodedeploy
