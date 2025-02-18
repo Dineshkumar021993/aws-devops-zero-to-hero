3d73d25bd817a4e5c2c3d4dcac8afed8a7b0d9a0
Dinesh kumar <90820982+Dineshkumar021993@users.noreply.github.com>
2025-02-18 15:01:16 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,4 +4,4 @@ set -e
 # Pull the Docker image from Docker Hub
 sudo docker pull dineshkumar021993/samplepythonawscodedeploy
 # Run the Docker image as a container
-sudo docker run -d -p 6000:9000 dineshkumar021993/samplepythonawscodedeploy
+sudo docker run -d -p 6000:8000 dineshkumar021993/samplepythonawscodedeploy
